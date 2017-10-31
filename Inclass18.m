af597aac5441e41c0d244f4a65a3482dd068a8a2
warmflasha <warmflasha@gmail.com>
2017-10-31 14:11:25 -0500

    Wrote inclass18

---
+++ Inclass18.m
@@ -0,0 +1,14 @@
+% In class 18
+
+% Problem 1. In this directory, you will find the same image of yeast cells as you used
+% in homework 5. First preprocess the image any way you like - smoothing, edge detection, etc. 
+% Then, try to find as many of the cells as you can using the
+% imfindcircles routine with appropriate parameters. 
+% Display the image with the circles drawn on it.  
+
+% Problem 2. (A) Draw two sets of 10 random numbers - one from the integers
+% between 1 and 9 and the second from the integers between 1 and 10. Run a
+% ttest to see if these are significantly different. (B) Repeat this a few
+% times with different sets of random numbers to see if you get the same
+% result. (C) Repeat (A) and (B) but this time use 100 numbers in each set
+% and then 1000 numbers in each set. Comment on the results. 
